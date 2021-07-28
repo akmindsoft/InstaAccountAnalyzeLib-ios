fd78a6ad80e90545907ba5c2053ee7c444f7763f
Ahmet Koçu <akocu@ziraatteknoloji.com>
2021-07-28 10:18:32 +0300

    name fix.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyPackage",
+    name: "InstaAccountAnalyzeLib",
     products: [
         .library(name: "InstaAccountAnalyzeLib", targets: ["InstaAccountAnalyzeLib"])
     ],
