613048801b40a5e4e54f5aa9a09b8d10c7ce29a6
Ahmet Koçu <akocu@ziraatteknoloji.com>
2021-07-28 10:15:02 +0300

    url fix.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "InstaAccountAnalyzeLib",
-            url: "https://github.com/akmindsoft/InstaAccountAnalyzeLib-ios/InstaAccountAnalyzeLib-1.1.2.xcframework.zip?raw=true",
+            url: "https://github.com/akmindsoft/InstaAccountAnalyzeLib-ios/blob/master/InstaAccountAnalyzeLib-1.1.2.xcframework.zip?raw=true",
             checksum: "d3ff8830e2353f7dae296f53d9ee21faf627eb7c9953a6a5b37656d29c1cf50e"
         )
     ]
